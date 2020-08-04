1aa525e95781668ca007ebdf0427298e67f12a7f
meri12345 <38717612+meri12345@users.noreply.github.com>
2020-08-04 23:30:08 +0200

    classes

--- index.php
+++ index.php
@@ -36,6 +36,31 @@ $task = [
 
 $animals=['dogs','cats'];
 
+class Task{
+
+    public $description;
+    public $completed=false;
+
+    public function __construct($description){
+        $this->description=$description;
+    }
+
+    public function isComplete(){
+        return $this->completed;
+    }
+
+    public function complete(){
+        $this->completed=true;
+    }
+
+};
+
+$tasks =[ new Task('Go to grocery'),
+        new Task('Do Homework'),
+        new Task('Sleep'),
+        new Task('Study'),];
+
+$tasks[0]->complete();
 
 require 'index.view.php';
 
--- index.view.php
+++ index.view.php
@@ -62,5 +62,25 @@
 		</li>
 	</ul>
 
+	<hr>
+
+	<h3>Extract data from class Task objects</h3>
+
+	<ul>
+			<?php foreach ($tasks as $task) : ?>
+				<li>
+				
+					<?php if($task->completed) : ?>
+					<strike><?=$task->description?></strike>
+
+					<?php else : ?>
+					<?=$task->description?>
+					
+					<?php endif ?>
+
+				</li>
+			<?php endforeach ?>
+	</ul>
+
 </body>
 </html>
