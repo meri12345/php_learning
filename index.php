bd1bc9de2856caa9744f4152a4c6de88f8048a1f
meri12345 <38717612+meri12345@users.noreply.github.com>
2020-08-04 21:56:46 +0200

    mysql

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 
+$user = 'root';
+$password = 'root';
+$db = 'inventory';
+$host = 'localhost';
+$port = 3306;
+
+$link = mysqli_init();
+$success = mysqli_real_connect(
+   $link, 
+   $host, 
+   $user, 
+   $password, 
+   $db,
+   $port
+);
+
 require 'functions.php';
 
 $person = [
